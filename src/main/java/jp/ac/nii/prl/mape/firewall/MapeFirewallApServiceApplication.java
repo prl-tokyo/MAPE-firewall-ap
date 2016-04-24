1d8a5b8365426e1758ec5d56851f07b7658a0100
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-24 23:04:35 +0900

    Import generated Spring project

---
+++ src/main/java/jp/ac/nii/prl/mape/firewall/MapeFirewallApServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.firewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeFirewallApServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeFirewallApServiceApplication.class, args);
+	}
+}
