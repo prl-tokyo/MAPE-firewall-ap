6b5157ddffa0d106fd14448420c955a0b5519594
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 11:03:59 +0900

    Rule repository

---
+++ src/main/java/jp/ac/nii/prl/mape/firewall/repository/RuleRepository.java
@@ -0,0 +1,15 @@
+package jp.ac.nii.prl.mape.firewall.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.ac.nii.prl.mape.firewall.model.Rule;
+
+public interface RuleRepository extends JpaRepository<Rule, Long> {
+
+	public Optional<Rule> findById(Long ruleId);
+	
+	public List<Rule> findByViewId(Long viewId);
+}
