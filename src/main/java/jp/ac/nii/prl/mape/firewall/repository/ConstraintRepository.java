19972fb5416337185fa4e87da283317cf3a3372b
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 12:07:39 +0900

    Find optional Constraint by ID

--- src/main/java/jp/ac/nii/prl/mape/firewall/repository/ConstraintRepository.java
+++ src/main/java/jp/ac/nii/prl/mape/firewall/repository/ConstraintRepository.java
@@ -1,9 +1,13 @@
 package jp.ac.nii.prl.mape.firewall.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jp.ac.nii.prl.mape.firewall.model.Constraint;
 
 public interface ConstraintRepository extends JpaRepository<Constraint, Long> {
+
+	Optional<Constraint> findById(Long constraintId);
 
 }
