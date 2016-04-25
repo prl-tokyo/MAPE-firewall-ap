0ccf77760b3f3a7ea1524ec2c22aa77b15082c03
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 11:26:25 +0900

    Simple REST controller
    Stores and retrieves views

---
+++ src/main/java/jp/ac/nii/prl/mape/firewall/controller/FirewallController.java
@@ -0,0 +1,62 @@
+package jp.ac.nii.prl.mape.firewall.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.undertow.attribute.RequestMethodAttribute;
+import jp.ac.nii.prl.mape.firewall.model.Rule;
+import jp.ac.nii.prl.mape.firewall.model.View;
+import jp.ac.nii.prl.mape.firewall.service.RuleService;
+import jp.ac.nii.prl.mape.firewall.service.ViewService;
+
+@RestController
+@Component
+@RequestMapping("/firewall")
+public class FirewallController {
+
+	private final ViewService viewService;
+	private final RuleService ruleService;
+	
+	@Autowired
+	public FirewallController(ViewService viewService, RuleService ruleService) {
+		this.viewService = viewService;
+		this.ruleService = ruleService;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<?> createView(@RequestBody View view) {
+		// persist view and rules
+		viewService.save(view);
+		for (Rule rule:view.getRules())
+			ruleService.save(rule);
+		
+		// create response
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(view.getId()).toUri());
+		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/{viewId}", method=RequestMethod.GET)
+	public View getView(@PathVariable Long viewId) {
+		Optional<View> view = viewService.findById(viewId);
+		
+		if (view.isPresent())
+			return view.get();
+		//else
+		throw new ViewNotFoundException(String.format("No view with id %s", viewId));
+	}
+	
+}
